c0cbeffd0a4975ae72c190642ec1ee6b39e03c34
Varun-Mandley <varunmandley@gmail.com>
2022-09-12 01:14:22 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Login from './Login';
-import Feedback from './Feedback';
+import React, { useState } from 'react';
+import './App.css'
+
 const App = () => {
+  const [records,setRecords]=useState([]);
+  const [data,setdata]=useState({no:''});
+  const handler=(e)=>{
+    setdata({...data,[e.target.name]:e.target.value})
+  };
+  // console.log(data.no);
+  const handlerAdd=()=>{
+    fetch('https://randomuser.me/api/?results='+ data.no).then((response)=>response.json()).then((data)=>{
+      // console.log(data.results);
+      setRecords(data.results)
+    })
+  };
   return (
     <div>
-        <Router>
-            <Switch>
-                <Route path="/" component={Login} exact></Route>
-                <Route path="/feedback/:fn" component={Feedback} ></Route>
-            </Switch>
-        </Router>
-        
+      <h2 className='tittle'>Generate Random User Data</h2>
+      <h2 className='tittle-data'><b>Number : </b><input type="text" name='no' value={data.no} onChange={handler}/></h2>
+      <p><input className='btn' type="button" value="Generate"  onClick={handlerAdd}/> </p>     
+      {
+        records.map((row,i)=>{return(
+          <div key={i} className="data">
+              <img className='user-img' src={row.picture.large} alt={row.name.first} />
+              <h2 className='name'>{row.name.first}&nbsp;{row.name.last} | {row.gender} </h2>
+              <p><b>Email : </b>{row.email}</p>
+              <p><b>Age :</b> {row.dob.age}</p>
+              <p><b>Mobile No :</b> {row.cell}</p>
+              <p><b>Address :</b> {row.location.street.number} {row.location.street.name}, {row.location.state}, {row.location.country}</p>
+
+          </div> 
+      )})
+      }
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import App from './App';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// ReactDOM.render(<App />,document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
